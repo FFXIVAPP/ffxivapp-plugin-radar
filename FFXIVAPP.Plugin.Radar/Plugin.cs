a5f3e316660c811215cfdd57e3cafc4f7c211a5c
Ryan Wilson <syndicated.life@gmail.com>
2015-04-11 21:39:45 -0700

    issue with locale binding before locale has values

--- FFXIVAPP.Plugin.Radar/Plugin.cs
+++ FFXIVAPP.Plugin.Radar/Plugin.cs
@@ -161,6 +161,7 @@ namespace FFXIVAPP.Plugin.Radar
 
         public TabItem CreateTab()
         {
+            Locale = LocaleHelper.Update(Constants.CultureInfo);
             var content = new ShellView();
             content.Loaded += ShellViewModel.Loaded;
             var tabItem = new TabItem
