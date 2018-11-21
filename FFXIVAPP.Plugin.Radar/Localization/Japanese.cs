1b82da6294a390a1230b3c4f9a7086d4718dc723
logue <logue@hotmail.co.jp>
2018-11-21 16:48:00 +0900

    Update Japanese Translation

--- FFXIVAPP.Plugin.Radar/Localization/Japanese.cs
+++ FFXIVAPP.Plugin.Radar/Localization/Japanese.cs
@@ -34,6 +34,20 @@ namespace FFXIVAPP.Plugin.Radar.Localization {
             "アレクトリオン",
             "クーレア",
 
+            // Heavensward Rank A
+            "ミルカ",
+            "リューバ",
+            "ブネ",
+            "アガトス",
+            "パイルラスタ",
+            "ワイバーンロード",
+            "機兵のスリップキンクス",
+            "ストラス",
+            "キャムパクティ",
+            "センチブロッサム"
+            "エンケドラス",
+            "シシウトゥル",
+
             // Stormblood Rank A
             "アンガダ",
             "アクラブアメル",
@@ -50,7 +64,7 @@ namespace FFXIVAPP.Plugin.Radar.Localization {
         };
 
         private static readonly List<string> RankB = new List<string> {
-            "死灰のアルビン",
+            "アシエン・アルビン",
             "バーバステル",
             "ブラッディ・マリー",
             "ダークヘルメット",
@@ -67,6 +81,20 @@ namespace FFXIVAPP.Plugin.Radar.Localization {
             "スティンギング・ソフィー",
             "ヴオコー",
             "ホワイトジョーカー",
+
+            // Heavensward Rank B
+            "アルティック",
+            "ギガントピテクス",
+            "グナース・コメットドローン",
+            "クルーゼ",
+            "リュキダス",
+            "オムニ",
+            "プテリゴトゥス",
+            "舞手のサヌバリ",
+            "スキタリス",
+            "スクオンク",
+            "スケアクロウ",
+            "テクスタ",
 
             // Stormblood Rank B
             "アスワング",
@@ -101,13 +129,21 @@ namespace FFXIVAPP.Plugin.Radar.Localization {
             "ヌニュヌウィ",
             "ゾーナ・シーカー",
             "アグリッパ",
+            
+            // Heavensward Rank S
+            "カイザーベヒーモス",
+            "ガンダルヴァ",
+            "セーンムルウ",
+            "ペイルライダー",
+            "レウクロッタ"
+            "極楽鳥",
 
             // Stormblood Rank S
+            "ボーンクローラー",
             "ガンマ",
             "オキナ",
             "オルガナ",
             "ソルト・アンド・ライト",
-            "ボーンクローラー",
             "ウドンゲ"
         };
 
@@ -117,71 +153,71 @@ namespace FFXIVAPP.Plugin.Radar.Localization {
         public static ResourceDictionary Context() {
             Dictionary.Clear();
             Dictionary.Add("radar_", "*PH*");
-            Dictionary.Add("radar_RadarWidgetHeader", "Radar Widget");
-            Dictionary.Add("radar_OpenNowButtonText", "ウィジェットを開く");
-            Dictionary.Add("radar_ResetPositionButtonText", "位置をリセットする");
-            Dictionary.Add("radar_EnableClickThroughHeader", "ウィジェット上のクリックを下に通す");
-            Dictionary.Add("radar_WidgetOpacityHeader", "ウィジェット透過率");
-            Dictionary.Add("radar_ShowTitlesOnRadarHeader", "Show Titles On Radar");
-            Dictionary.Add("radar_UIScaleHeader", "UI Scale");
-            Dictionary.Add("radar_RadarSettingsTabHeader", "Radar Settings");
-            Dictionary.Add("radar_PCShowHeader", "PC Show");
-            Dictionary.Add("radar_PCShowNameHeader", "PC Show Name");
-            Dictionary.Add("radar_PCShowHPPercentHeader", "PC Show HP Percent");
-            Dictionary.Add("radar_PCShowJobHeader", "PC Show Job");
-            Dictionary.Add("radar_PCShowDistanceHeader", "PC Show Distance");
-            Dictionary.Add("radar_NPCShowHeader", "NPC Show");
-            Dictionary.Add("radar_NPCShowNameHeader", "NPC Show Name");
-            Dictionary.Add("radar_NPCShowHPPercentHeader", "NPC Show HP Percent");
-            Dictionary.Add("radar_NPCShowDistanceHeader", "NPC Show Distance");
-            Dictionary.Add("radar_MonsterShowHeader", "Monster Show");
-            Dictionary.Add("radar_MonsterShowNameHeader", "Monster Show Name");
-            Dictionary.Add("radar_MonsterShowHPPercentHeader", "Monster Show HP Percent");
-            Dictionary.Add("radar_MonsterShowDistanceHeader", "Monster Show Distance");
-            Dictionary.Add("radar_GatheringShowHeader", "Gathering Show");
-            Dictionary.Add("radar_GatheringShowNameHeader", "Gathering Show Name");
-            Dictionary.Add("radar_GatheringShowHPPercentHeader", "Gathering Show HP Percent");
-            Dictionary.Add("radar_GatheringShowDistanceHeader", "Gathering Show Distance");
-            Dictionary.Add("radar_OtherShowHeader", "Other Show");
-            Dictionary.Add("radar_OtherShowNameHeader", "Other Show Name");
-            Dictionary.Add("radar_OtherShowHPPercentHeader", "Other Show HP Percent");
-            Dictionary.Add("radar_OtherShowDistanceHeader", "Other Show Distance");
-            Dictionary.Add("radar_GitHubButtonText", "プロジェクトソースを開く(GitHub)");
-            Dictionary.Add("radar_PCFontSizeHeader", "PC Font Size");
-            Dictionary.Add("radar_PCFontColorHeader", "PC Font Color");
-            Dictionary.Add("radar_NPCFontSizeHeader", "NPC Font Size");
-            Dictionary.Add("radar_NPCFontColorHeader", "NPC Font Color");
-            Dictionary.Add("radar_MonsterFontSizeHeader", "Monster Font Size");
-            Dictionary.Add("radar_MonsterFontColorHeader", "Monster Font Color");
-            Dictionary.Add("radar_GatheringFontSizeHeader", "Gathering Font Size");
-            Dictionary.Add("radar_GatheringFontColorHeader", "Gathering Font Color");
-            Dictionary.Add("radar_OtherFontSizeHeader", "Other Font Size");
-            Dictionary.Add("radar_OtherFontColorHeader", "Other Font Color");
+            Dictionary.Add("radar_RadarWidgetHeader", "レーダーウィジェット");
+            Dictionary.Add("radar_OpenNowButtonText", "開く");
+            Dictionary.Add("radar_ResetPositionButtonText", "設定を初期化");
+            Dictionary.Add("radar_EnableClickThroughHeader", "Enable Click-Through On Radar");
+            Dictionary.Add("radar_WidgetOpacityHeader", "ウィジェットの透過度");
+            Dictionary.Add("radar_ShowTitlesOnRadarHeader", "レーダーにタイトルを表示");
+            Dictionary.Add("radar_UIScaleHeader", "UI拡大率");
+            Dictionary.Add("radar_RadarSettingsTabHeader", "レーダー設定");
+            Dictionary.Add("radar_PCShowHeader", "プレイヤーの表示");
+            Dictionary.Add("radar_PCShowNameHeader", "プレイヤーの名前を表示する");
+            Dictionary.Add("radar_PCShowHPPercentHeader", "プレイヤーのHPバーを表示する");
+            Dictionary.Add("radar_PCShowJobHeader", "プレイヤーのジョブを表示する");
+            Dictionary.Add("radar_PCShowDistanceHeader", "プレイヤーを表示する距離");
+            Dictionary.Add("radar_NPCShowHeader", "NPCの表示");
+            Dictionary.Add("radar_NPCShowNameHeader", "NPCの名前を表示");
+            Dictionary.Add("radar_NPCShowHPPercentHeader", "NPCのHP（％）を表示");
+            Dictionary.Add("radar_NPCShowDistanceHeader", "NPCを表示する距離");
+            Dictionary.Add("radar_MonsterShowHeader", "モンスター表示");
+            Dictionary.Add("radar_MonsterShowNameHeader", "モンスターの名前を表示");
+            Dictionary.Add("radar_MonsterShowHPPercentHeader", "モンスターのHP（％）を表示");
+            Dictionary.Add("radar_MonsterShowDistanceHeader", "モンスターの表示距離");
+            Dictionary.Add("radar_GatheringShowHeader", "採集（ギャザリング）の表示");
+            Dictionary.Add("radar_GatheringShowNameHeader", "採集場所の名前");
+            Dictionary.Add("radar_GatheringShowHPPercentHeader", "採集場所のHP（％）を表示");
+            Dictionary.Add("radar_GatheringShowDistanceHeader", "採集場所を表示する距離");
+            Dictionary.Add("radar_OtherShowHeader", "その他の表示");
+            Dictionary.Add("radar_OtherShowNameHeader", "その他の名前を表示する");
+            Dictionary.Add("radar_OtherShowHPPercentHeader", "その他のHP（％）を表示");
+            Dictionary.Add("radar_OtherShowDistanceHeader", "その他を表示する距離");
+            Dictionary.Add("radar_GitHubButtonText", "プロジェクトのソースを開く（GitHub）");
+            Dictionary.Add("radar_PCFontSizeHeader", "プレイヤーの文字サイズ");
+            Dictionary.Add("radar_PCFontColorHeader", "プレイヤーの文字色");
+            Dictionary.Add("radar_NPCFontSizeHeader", "NPCの文字サイズ");
+            Dictionary.Add("radar_NPCFontColorHeader", "NPCの文字色");
+            Dictionary.Add("radar_MonsterFontSizeHeader", "モンスターの文字サイズ");
+            Dictionary.Add("radar_MonsterFontColorHeader", "モンスターの文字色");
+            Dictionary.Add("radar_GatheringFontSizeHeader", "採集場所の文字サイズ");
+            Dictionary.Add("radar_GatheringFontColorHeader", "採集場所の文字色");
+            Dictionary.Add("radar_OtherFontSizeHeader", "その他の文字サイズ");
+            Dictionary.Add("radar_OtherFontColorHeader", "その他の文字色");
 
-            Dictionary.Add("radar_MonsterShowRankColorHeader", "Show Ranked Monsters In Color");
-            Dictionary.Add("radar_MonsterShowBRankHeader", "Monster Show (B Rank)");
-            Dictionary.Add("radar_MonsterShowARankHeader", "Monster Show (A Rank)");
-            Dictionary.Add("radar_MonsterShowSRankHeader", "Monster Show (S Rank)");
-            Dictionary.Add("radar_MonsterFontColorBRankHeader", "Monster Font Color (B Rank)");
-            Dictionary.Add("radar_MonsterFontColorARankHeader", "Monster Font Color (A Rank)");
-            Dictionary.Add("radar_MonsterFontColorSRankHeader", "Monster Font Color (S Rank)");
-            Dictionary.Add("radar_FilterOptionsHeader", "Filter Options");
-            Dictionary.Add("radar_KeyLabel", "Key:");
-            Dictionary.Add("radar_LevelLabel", "Level:");
-            Dictionary.Add("radar_TypeLabel", "Type:");
+            Dictionary.Add("radar_MonsterShowRankColorHeader", "ランク付きモンスターを色別に表示");
+            Dictionary.Add("radar_MonsterShowBRankHeader", "モンスターの表示（Bランク）");
+            Dictionary.Add("radar_MonsterShowARankHeader", "モンスターの表示（Aランク）");
+            Dictionary.Add("radar_MonsterShowSRankHeader", "モンスターの表示（Sランク）");
+            Dictionary.Add("radar_MonsterFontColorBRankHeader", "モンスターの文字色（Bランク）");
+            Dictionary.Add("radar_MonsterFontColorARankHeader", "モンスターの文字色（Aランク）");
+            Dictionary.Add("radar_MonsterFontColorSRankHeader", "モンスターの文字色（Sランク）");
+            Dictionary.Add("radar_FilterOptionsHeader", "フィルタオプション");
+            Dictionary.Add("radar_KeyLabel", "キーワード：");
+            Dictionary.Add("radar_LevelLabel", "レベル：");
+            Dictionary.Add("radar_TypeLabel", "種類：");
             Dictionary.Add("radar_DeleteFilterButton", " - ");
-            Dictionary.Add("radar_AddorUpdateFilterButton", "Add Or Update Filter");
-            Dictionary.Add("radar_SettingsRadarHeader", "Settings:Radar");
-            Dictionary.Add("radar_CompasModeLabel", "Compass Mode");
-            Dictionary.Add("radar_FilterRadarItemsLabel", "Filter Radar Items");
-            Dictionary.Add("radar_ShowEntityDebugLabel", "Show Entity Debug Information");
-            Dictionary.Add("radar_RadarUIScaleLabel", "UI Scale");
-            Dictionary.Add("radar_SettingsRadarPCHeader", "Settings:Radar:PC");
-            Dictionary.Add("radar_SettingsRadarNPCHeader", "Settings:Radar:NPC");
-            Dictionary.Add("radar_SettingsRadarMonsterHeader", "Settings:Radar:Monster");
-            Dictionary.Add("radar_SettingsRadarGatheringHeader", "Settings:Radar:Gathering");
-            Dictionary.Add("radar_SettingsRadarOtherHeader", "Settings:Radar:Other");
-            Dictionary.Add("radar_RadarTitleBar", "[RADAR]");
+            Dictionary.Add("radar_AddorUpdateFilterButton", "フィルタの追加もしくは更新");
+            Dictionary.Add("radar_SettingsRadarHeader", "設定：レーダーr");
+            Dictionary.Add("radar_CompasModeLabel", "コンパスモード");
+            Dictionary.Add("radar_FilterRadarItemsLabel", "レーダー項目のフィルタ");
+            Dictionary.Add("radar_ShowEntityDebugLabel", "エンティティのデバッグ情報を表示");
+            Dictionary.Add("radar_RadarUIScaleLabel", "UI拡大率");
+            Dictionary.Add("radar_SettingsRadarPCHeader", "設定：レーダー：プレイヤー");
+            Dictionary.Add("radar_SettingsRadarNPCHeader", "設定：レーダー：NPC");
+            Dictionary.Add("radar_SettingsRadarMonsterHeader", "設定：レーダー：モンスター");
+            Dictionary.Add("radar_SettingsRadarGatheringHeader", "設定：レーダー：採集場所");
+            Dictionary.Add("radar_SettingsRadarOtherHeader", "設定」レーダー：その他");
+            Dictionary.Add("radar_RadarTitleBar", "[レーダー]");
 
             return Dictionary;
         }
